8678e5d3abdae5aa4a0a092fa14002fd67ff511b
mishasro72 <mishasro72@gmail.com>
2025-12-21 13:29:25 -0500

    Add tasks 22-24

--- code_challenges.js
+++ code_challenges.js
@@ -748,7 +748,6 @@ function getSaleItems(data) {
 let shoppingCarts = getSaleItems(products);
 console.log(shoppingCarts);
 
-
 /* Task 18 Total the Groceries 
 Use reduce() to total the groceries. 
 Then find a method that will round the total to 2 decimal places.
@@ -758,17 +757,11 @@ Example output: 73.44
 
 import shoppingCart from "./data_groceries.js";
 
-function total(arr){
-
-  return arr
-    .reduce((sum, { price }) => sum + price, 0)
-    .toFixed(2);
-    
+function total(arr) {
+  return arr.reduce((sum, { price }) => sum + price, 0).toFixed(2);
 }
 
 console.log(total(shoppingCart));
-
-
 
 /* Task 19 Total the Savory Items
 Use reduce() and only reduce() to calculate and return 
@@ -779,15 +772,17 @@ Expected output: 9.97
 */
 // import shoppingCart from "./data_groceries.js";
 
-function totalSavory(arr){
-  return Number (arr
-    .reduce((sum, item) => {
-      if(item.type == 'savory') {
-        sum += item.price;
-      }
-      return sum;
-    }, 0)
-    .toFixed(2));
+function totalSavory(arr) {
+  return Number(
+    arr
+      .reduce((sum, item) => {
+        if (item.type == "savory") {
+          sum += item.price;
+        }
+        return sum;
+      }, 0)
+      .toFixed(2)
+  );
 }
 
 console.log(totalSavory(shoppingCart));
@@ -810,16 +805,14 @@ console.log(totalSavory(shoppingCart));
 */
 import products1 from "./data_products.js";
 
-function sortProducts(data){
+function sortProducts(data) {
   return data.sort((a, b) => a.price - b.price);
 }
-  
-
 
 const listByCheapest = sortProducts(products1);
 for (let item of listByCheapest) {
   console.log(item.product + "," + item.price);
-};
+}
 
 /* Task 21 Find All Unique Tags 
 
@@ -835,44 +828,162 @@ Expected output:
 ["supernatural", "horror", "drama",
 "fantasy", "reality", "home improvement", "comedy", "sci-fi", "adventure"]
 
-*/ 
+*/
 
 import mediaData from "./data_media.js";
 
 //version 1 using Set and flat()
-function getUniqueTags(data){
-  let newTags = [];  
-  data.forEach(item => {
-    newTags.push(item['tags']);
-  }); 
+function getUniqueTags(data) {
+  let newTags = [];
+  data.forEach((item) => {
+    newTags.push(item["tags"]);
+  });
   return [...new Set(newTags.flat())];
-  }  
-  
+}
+
 console.log(getUniqueTags(mediaData));
 
 //version 2 using Set and reduce()
-function getUniqueTags1(data){
-  return [...data.reduce((acc, { tags }) => {
-    tags.forEach(tag => acc.add(tag));
-    return acc;
-  }, new Set())
+function getUniqueTags1(data) {
+  return [
+    ...data.reduce((acc, { tags }) => {
+      tags.forEach((tag) => acc.add(tag));
+      return acc;
+    }, new Set()),
   ];
-}  
-  
+}
+
 console.log(getUniqueTags1(mediaData));
 
 //version 3 using for of loop
-function getUniqueTags2(data){
-  let tags = data.map(item => item.tags).flat();
+function getUniqueTags2(data) {
+  let tags = data.map((item) => item.tags).flat();
   let uniqTags = [];
 
-  tags.forEach(tag => {
-    if(!uniqTags.includes(tag)) {
+  tags.forEach((tag) => {
+    if (!uniqTags.includes(tag)) {
       uniqTags.push(tag);
     }
-  })
+  });
 
   return uniqTags;
 }
 
 console.log(getUniqueTags2(mediaData));
+
+/* Task 22 Welcome Aboard Scrimba Airlines 
+
+Our Scrimba Airlines in-flight entertainment package 
+includes a variety of podcasts. We need to add a feature that suggests
+podcasts to our patrons based on whether a flight is short or long. 
+
+Your sort function should take two arguments: the podcast data and
+flight length. If the flight is 60 minutes or less, sort the podcast list 
+from shortest to longest. If it's anything else, sort from longest
+to shortest. 
+
+Your function shouldn't return anything. Instead log a numbered list 
+of the title and duration of 
+each podcast to the console, like this:
+
+1. Crime Fan, 150 minutes
+2. Mythical Creatures, 99 minutes
+3. Crime Crime Crime, 70 minutes
+4. Coding Corner, 55 minutes
+5. Scrimba Podcast, 50 minutes
+6. Something about Witches, 35 minutes
+
+*/
+
+import podcasts1 from "./data_podcast.js";
+
+function sortByDuration(data, flightLength) {
+  if (flightLength <= 60) {
+    data.sort((a, b) => a.duration - b.duration);
+  } else {
+    data.sort((a, b) => b.duration - a.duration);
+  }
+  data.forEach(({ title, duration }, index) =>
+    console.log(`${index + 1}. ${title}, ${duration} minutes`)
+  );
+}
+
+sortByDuration(podcasts1, 61);
+
+/* Task 23 Popularity Contest 
+
+Iggy the Influencer and Toby the Tiktoker are dying to know
+who's more popular on social media. 
+
+Toby's TikToks get an average of 400 likes. On average, how many
+likes do Iggy's Instagram posts get? 
+
+In data.js you'll find a list of Iggy's recent posts. 
+Use reduce() to write a function that returns the average number of likes.
+To find the average, add up the total number of likes, then divide
+by the total number of posts.
+*/
+
+import postData from "./data_posts.js";
+
+function calcAverageLikes(data) {
+  return Math.floor(
+    data.reduce((acc, curr) => acc + curr.likes, 0) / data.length
+  );
+}
+
+console.log(calcAverageLikes(postData));
+
+/* Task 24 Night at the Scrimbies 
+
+It's time for the Scrimbies, a prestigious award show for podcast hosts.
+We need to assemble a list of podcast hosts so we can start handing out awards. 
+
+Write a function that takes in the podcast data and
+returns a flat array of podcast hosts. There are quite a few ways to approach
+this, but try solving the problem using reduce(). 
+
+Once you have a flat array of hosts, write a second function to randomly assign each host a prize
+from the awards array. 
+
+Example output: ["🏆 Alex Booker", "⭐ Bob Smith", "💎 Camilla Lambert" ...] 
+
+*/
+
+//version 1 with push
+const awards = ["🏆", "⭐", "💎", "🥇", "👑"];
+
+function getHosts(data) {
+  return data.reduce((acc, podcast) => {
+    acc.push(podcast.hosts);
+    return acc.flat(); //because push returns the new length of the array
+  }, []);
+}
+
+function assignAwards(data) {
+  let hostsPrize = [];
+  getHosts(data).forEach(item => {
+    hostsPrize.push(`${awards[Math.floor(Math.random() * 5)]} ${item}`)
+  });
+  return hostsPrize;
+}
+
+console.log(getHosts(podcasts1));
+console.log(assignAwards(podcasts1));
+
+//version 2 with concat
+function getHosts1(data) {
+  return data.reduce((acc, podcast) => acc.concat(podcast.hosts), new Array());
+}
+
+function assignAwards1(data) {
+  let hostsPrize = [];
+  getHosts(data).forEach(item => {
+    hostsPrize = hostsPrize.concat(`${awards[Math.floor(Math.random() * 5)]} ${item}`);
+    console.log(hostsPrize);
+  });
+  return hostsPrize;
+}
+
+console.log(getHosts1(podcasts1));
+console.log(assignAwards1(podcasts1));
